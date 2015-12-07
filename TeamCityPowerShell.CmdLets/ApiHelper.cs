4d132e8e26ea8d63f42c2a7edb818cb0d85c1359
Jia Qiu <jia.qiu@ibiz-solutions.se>
2015-12-07 11:52:21 +0100

    Added code for unzip and zip-file removal

--- TeamCityPowerShell.CmdLets/ApiHelper.cs
+++ TeamCityPowerShell.CmdLets/ApiHelper.cs
@@ -4,6 +4,8 @@ using System.IO;
 using System.Net;
 using System.Net.Http;
 using System.Text;
+using ICSharpCode.SharpZipLib.Core;
+using ICSharpCode.SharpZipLib.Zip;
 
 namespace TeamCityPowerShell.CmdLets
 {
@@ -46,11 +48,56 @@ namespace TeamCityPowerShell.CmdLets
                            ".tcbuildtag/.lastPinned");
         }
 
-        public void SaveArtifacts(byte[] artifacts, string buildConfiguration, string tag, string fileName)
+        public void SaveArtifacts(byte[] artifacts, string buildConfiguration, string tag, string fileName, bool unzip)
         {
             var di = Directory.CreateDirectory(Path.Combine(DeployPath, buildConfiguration, tag));
 
             File.WriteAllBytes(Path.Combine(di.FullName, fileName), artifacts);
+
+            if (unzip)
+            {
+                ExtractZipFile(Path.Combine(di.FullName, fileName), di.FullName.ToString());
+                File.Delete(Path.Combine(di.FullName, fileName));
+            }
+        }
+
+
+        public void ExtractZipFile(string archiveFilenameIn, string outFolder)
+        {
+            ZipFile zf = null;
+            try
+            {
+                FileStream fs = File.OpenRead(archiveFilenameIn);
+                zf = new ZipFile(fs);
+                foreach (ZipEntry zipEntry in zf)
+                {
+                    if (!zipEntry.IsFile)
+                    {
+                        continue;
+                    }
+                    string entryFileName = zipEntry.Name;
+                    byte[] buffer = new byte[4096];
+                    Stream zipStream = zf.GetInputStream(zipEntry);
+
+                    string fullZipToPath = Path.Combine(outFolder, entryFileName);
+                    string directoryName = Path.GetDirectoryName(fullZipToPath);
+                    if (directoryName.Length > 0)
+                        Directory.CreateDirectory(directoryName);
+
+                    using (FileStream streamWriter = File.Create(fullZipToPath))
+                    {
+                        StreamUtils.Copy(zipStream, streamWriter, buffer);
+                    }
+                }
+            }
+            finally
+            {
+                if (zf != null)
+                {
+                    zf.IsStreamOwner = true;
+                    zf.Close();
+                }
+            }
         }
     }
 }
